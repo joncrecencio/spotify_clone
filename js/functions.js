86acb7adfc7d075fc7e40d6053edcf4e3e77bd8f
Jonathan Crecencio <jonathancrecencio@outlook.com>
2020-09-11 02:29:01 -0300

    Usando o Javascript para tocar música

--- js/functions.js
+++ js/functions.js
@@ -2,6 +2,7 @@ const playListsSidebar = document.querySelector(".sidebar__playlists")
 const musicasParadas = document.querySelector(".main__row")
 const musicasPlaylists = document.querySelector("#musicas__tocadas")
 
+
 const playlistPrimeira = [
     {
         nome: "Playlist - Favoritas"
@@ -27,57 +28,72 @@ const imagensMusicas = [
     {
         urlImage:"../img/top50.png",
         nameImage:"As 50 mais tocadas",
+        dataSong: "Top 50",
+        dataFiles: "../assets/perfect_edsheeran.mp3",
         titleMusic:"As 50 mais tocadas",
         descriptionMusic: "Ouça agora as 50 mais tocadas"
     },
 
     {
         urlImage:"../img/edsheeran.jpg",
-        nameImage:"Ed Sheeran - Perfect",
-        titleMusic:"Divide",
+        nameImage:"Ed Sheeran",
+        dataSong: "Perfect",
+        dataFiles: "../assets/perfect_edsheeran.mp3",
+        titleMusic:"Perfect",
         descriptionMusic: "Ouça agora Perfect de Ed Sheeran"
     },
 
     {
         urlImage:"../img/gunsrools.png",
         nameImage:"Guns N' Roses",
+        dataSong: "Sweet Child Of Mine",
+        dataFiles: "../assets/music.mp3",
         titleMusic:"Sweet Child Of Mine",
         descriptionMusic: "Ouça agora o álbum de Guns N' Roses"
     },
     {
-        urlImage:"../img/top50global.png",
+        urlImage:"../img/top50.png",
         nameImage:"As 50 mais tocadas",
+        dataSong: "Top 50",
+        dataFiles: "../assets/perfect_edsheeran.mp3",
         titleMusic:"As 50 mais tocadas",
         descriptionMusic: "Ouça agora as 50 mais tocadas"
     },
 
     {
         urlImage:"../img/edsheeran.jpg",
-        nameImage:"Ed Sheeran - Perfect",
-        titleMusic:"Divide",
+        nameImage:"Ed Sheeran",
+        dataSong: "Perfect",
+        dataFiles: "../assets/perfect_edsheeran.mp3",
+        titleMusic:"Perfect",
         descriptionMusic: "Ouça agora Perfect de Ed Sheeran"
     },
 
     {
         urlImage:"../img/gunsrools.png",
         nameImage:"Guns N' Roses",
+        dataSong: "Sweet Child Of Mine",
+        dataFiles: "../assets/music.mp3",
         titleMusic:"Sweet Child Of Mine",
         descriptionMusic: "Ouça agora o álbum de Guns N' Roses"
     },
     {
-        urlImage:"../img/top50global.png",
+        urlImage:"../img/top50.png",
         nameImage:"As 50 mais tocadas",
+        dataSong: "Top 50",
+        dataFiles: "../assets/perfect_edsheeran.mp3",
         titleMusic:"As 50 mais tocadas",
         descriptionMusic: "Ouça agora as 50 mais tocadas"
     },
 
     {
         urlImage:"../img/edsheeran.jpg",
-        nameImage:"Ed Sheeran - Perfect",
-        titleMusic:"Divide",
+        nameImage:"Ed Sheeran",
+        dataSong: "Perfect",
+        dataFiles: "../assets/perfect_edsheeran.mp3",
+        titleMusic:"Perfect",
         descriptionMusic: "Ouça agora Perfect de Ed Sheeran"
     },
-
 ]
 
 const imagensMusicasPlaylist = [
@@ -108,7 +124,8 @@ function renderPlaylistsFavorite(){
 
 function renderParadas(){
         imagensMusicas.forEach((photoAlbum) => {
-            musicasParadas.innerHTML += `<div class="main__col"><img src="${photoAlbum.urlImage}" alt="${photoAlbum.nameImage}">
+            musicasParadas.innerHTML += `<div class="main__col" data-image = "${photoAlbum.urlImage}" data-artist = "${photoAlbum.nameImage}" data-song = "${photoAlbum.dataSong}" data-file = "${photoAlbum.dataFiles}" >
+            <img src="${photoAlbum.urlImage}" alt="${photoAlbum.nameImage}">
             <h3>${photoAlbum.titleMusic}</h3>
             <p>${photoAlbum.descriptionMusic}</p></div>`
     })
@@ -129,4 +146,62 @@ function renderMusicasTocadas(){
 renderPlaylistsFavorite()
 renderParadas()
 renderMusicasTocadas()
+
+const audioPlayer = document.querySelector("#audioplayer")
+var loaded = false;
+const playBtn = document.querySelector("#playBtn")
+const pauseBtn = document.querySelector("#pauseBtn")
+
+pauseBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    playBtn.style.display = "inline"
+    pauseBtn.style.display = "none"
+    audioPlayer.pause()
+    return false
+})
+
+playBtn.addEventListener('click',(e)=>{
+    e.preventDefault()
+
+    playBtn.style.display = "none"
+    pauseBtn.style.display = "inline"
+    audioPlayer.play()
+    return false
+})
+
+const playSong = (file) => {
+    if(loaded == false){
+        audioPlayer.innerHTML = `<source src="${file}" type="audio/mp3" />`
+        loaded = true
+    }
+    playBtn.style.display = "none";
+    pauseBtn.style.display = "inline";
+
+    audioPlayer.load()
+
+}
+
+document.querySelectorAll('.main__col').forEach(item =>{
+
+    item.addEventListener('click', event=>{
+        let image = [item.getAttribute('data-image')]
+        let artist = [item.getAttribute('data-artist')]
+        let song = [item.getAttribute('data-song')]
+        let file = [item.getAttribute('data-file')]
+
+        var playerArtistComponent = document.getElementsByClassName('player__artist')
+
+            playerArtistComponent[0].innerHTML = `
+            <img src="${image}" />
+            <h3>${artist}<br/><span>${song}</span></h3>
+
+        `
+        console.log(playerArtistComponent)
+        playSong(file)
+    })
+
+})
+
+
+
 
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Clone Spotify
+
+Criado clone para fins de estudos do Javascript Puro
+
+## Tecnologias utilizadas
+- HTML
+- CSS
+- Javascript
+
+### Bibliotecas externas
+
+- Font awesome
+- Google Fonts
